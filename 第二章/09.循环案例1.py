bc6fe2e6a70244dbf681bb2b17d1faaa716b687d
luotaoping <luotaoping@pudu.com>
2026-03-26 16:08:53 +0800

    完成循环案例

--- 第二章/09.循环案例1.py
+++ 第二章/09.循环案例1.py
@@ -33,15 +33,3 @@ for char in myStr:
     elif char == "k":
         kCount += 1
 print(f"a的数量为：{aCount}，k的数量为：{kCount}")
-
-# 3.猜数字游戏
-randomNum = random.randint(1,101)
-while True:
-    num = int(input("请输入一个1~100的数字："))
-    if num == randomNum:
-        print("恭喜你猜对了")
-        break
-    elif num > randomNum:
-        print("你猜大了继续加油")
-    else:
-        print("你猜小了继续加油")

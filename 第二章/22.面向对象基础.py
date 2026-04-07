a35c2050467327f57749db2562a031ab1fea9908
luotaoping <luotaoping@pudu.com>
2026-04-07 11:59:10 +0800

    完成部分的类

--- 第二章/22.面向对象基础.py
+++ 第二章/22.面向对象基础.py
@@ -1,5 +1,9 @@
 # 定义类
 class Car:
+    # 类属性,所有示例共享,可直接用类名调用
+    wheel = 4
+    rate = 0.1  # 税率
+
     def __init__(self, brand, color, price):
         """
         初始化方法
@@ -10,15 +14,15 @@ class Car:
         self.brand = brand
         self.color = color
         self.price = price
+        self.rate = 0.12
 
-    def price_count(self, discount, rate):
+    def price_count(self, discount):
         """
         购车总价
         :param discount: 折扣
-        :param rate: 税率
         :return: 总价
         """
-        return self.price * (discount + rate)
+        return self.price * (discount + self.rate)
 
     def __str__(self):
         return f"{self.brand} {self.color} {self.price}"
@@ -49,9 +53,13 @@ car = Car("Xiaomi", "宝石绿", 259800)
 print(car)  # 未复写__str__打印的内存地址
 print(car.__dict__)  # 打印属性与值的字典键值对
 
-print(f"购车总价:{car.price_count(discount=0.8, rate=0.1)}")
+print(f"购车总价:{car.price_count(discount=0.8)}")
 
 car2 = Car("XiaoPeng", "灰色", 229800)
 print(car2)  # 复写__str__打印的是__str__中的实现
 
 print(car > car2)
+
+print(Car.wheel)
+print(Car.rate)  # 0.1
+print(car.rate)  # 使用示例调用时,示例属性优先于类属性 0.12

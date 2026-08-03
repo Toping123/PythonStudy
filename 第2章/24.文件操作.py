db43aacac7096143c5134b93ef8dfe1f38ce54f9
luotaoping <luotaoping@pudu.com>
2026-08-03 11:16:58 +0800

    绝对路径的文件操作

--- 第2章/24.文件操作.py
+++ 第2章/24.文件操作.py
@@ -1,25 +1,32 @@
 import json
 
 
-def write_file() -> None:
-    """
-    写入文件
-    """
+def write_file():
+    """相对路径写入文件"""
     with open("./Toping.txt", "w", encoding="UTF-8") as f:
         f.write("今天星期四\n")
         f.write("又是美好的一天")
 
 
-def read_file() -> None:
-    """
-    读取文件
-    :return:
-    """
+def read_file():
+    """相对路径读取文件"""
     with open("./Toping.txt", "r", encoding="UTF-8") as f:
+        print(f.read())
+        
+def write_absolute_file():
+    """绝对路径写入文件"""
+    with open("C:/Users/luotaoping/Desktop/Toping.txt", "w", encoding="UTF-8") as f:
+        f.write("今天星期四\n")
+        f.write("又是美好的一天")
+
+
+def read_absolute_file():
+    """绝对路径读取文件"""
+    with open("C:/Users/luotaoping/Desktop/Toping.txt", "r", encoding="UTF-8") as f:
         print(f.read())
 
 
-def write_json_file() -> None:
+def write_json_file():
     """
     写入Json数据到文件中
     :return:
@@ -29,7 +36,7 @@ def write_json_file() -> None:
         json.dump(user, f, ensure_ascii=False, indent=4)
 
 
-def read_json_file() -> None:
+def read_json_file():
     """
     读取文件并转为Json
     :return:
@@ -44,5 +51,7 @@ def read_json_file() -> None:
 if __name__ == '__main__':
     # write_file()
     # read_file()
-    write_json_file()
-    read_json_file()
+    write_absolute_file()
+    read_absolute_file()
+    # write_json_file()
+    # read_json_file()
